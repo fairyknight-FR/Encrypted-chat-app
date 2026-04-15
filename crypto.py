83f1ca12af6d032d5d68f9044043e19c6c5ba7f0
fairyknight-FR <fairyknight210@gmail.com>
2026-04-15 09:56:20 +0530

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -1,14 +1,13 @@
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 import os
 
-# Pre-shared key (must be SAME in client & server)
-KEY = b'12345678901234567890123456789012'  # 32 bytes = AES-256
+KEY = b'12345678901234567890123456789012'  # 32 bytes
 
 def encrypt_message(message: str) -> bytes:
     aesgcm = AESGCM(KEY)
-    nonce = os.urandom(12)  # Secure IV
+    nonce = os.urandom(12)
     ciphertext = aesgcm.encrypt(nonce, message.encode(), None)
-    return nonce + ciphertext  # send nonce + encrypted data
+    return nonce + ciphertext
 
 def decrypt_message(data: bytes) -> str:
     aesgcm = AESGCM(KEY)
